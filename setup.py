10d69adca23c73f43772d4dbfd940866b8994a8e
Sienna Bruinsma <sbruinsma@berkeley.edu>
2020-02-24 16:32:02 -0800

    changed setup.py and deleted some files

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='experiment_code',
     packages=find_packages(),
     version='0.1.0',
     description='language cort project',
