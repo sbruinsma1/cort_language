1601d5c9db253c65b904f380cf0e417aa4aa137b
Sienna Bruinsma <sbruinsma@berkeley.edu>
2020-04-06 00:46:45 -0700

    modifications to visualize.py to accomodate both datasets and the concatenated form

--- experiment_code/visualization/visualize.py
+++ experiment_code/visualization/visualize.py
@@ -27,7 +27,7 @@ def participant_version_count(dataframe, version):
     dataframe_version.Participant_Private_ID.unique()
     
     plt.figure(figsize=(10,10));
-    sns.countplot(x='Response', hue='Participant_Private_ID', data= dataframe_version);
+    sns.countplot(x='CoRT', hue='participant_id', data= dataframe_version);
     plt.xlabel('Response', fontsize=20)
     plt.ylabel('count', fontsize=20)
     plt.title('Number of responses per version', fontsize=20);
@@ -63,7 +63,7 @@ def cort_scores_count(dataframe):
     """
     plt.figure(figsize=(10,10))
 
-    sns.countplot(x='Response', data=dataframe)
+    sns.countplot(x='CoRT', data=dataframe)
     plt.xlabel('CoRT Scaling', fontsize=20)
     plt.ylabel('count', fontsize=20)
     plt.title('Number of responses across scores', fontsize=20);
@@ -79,7 +79,7 @@ def cort_scores_version_count(dataframe):
             plots score count per cort value per version
     """
     plt.figure(figsize=(10,10))
-    ax = sns.countplot(x='Response', hue='version', data=dataframe)
+    ax = sns.countplot(x='CoRT', hue='version', data=dataframe)
     ax.legend(loc='best', bbox_to_anchor=(1,1))
     plt.xlabel('CoRT Scaling', fontsize=20)
     plt.ylabel('count', fontsize=20)
@@ -114,7 +114,7 @@ def cort_scores_mode(dataframe):
             plots mode of scores per cort value
     """
     plt.figure(figsize=(10,10))
-    x = dataframe.groupby('version').apply(lambda x: x[['Response']].mode()).reset_index()
+    x = dataframe.groupby('version').apply(lambda x: x[['CoRT']].mode()).reset_index()
     sns.barplot(x=x['version'], y=x['Response']);
     plt.xlabel('version', fontsize=20)
     plt.ylabel('mode of CoRT scores', fontsize=20)
@@ -130,7 +130,7 @@ def cloze_distribution(dataframe):
     """
     plt.figure(figsize=(10,10))
 
-    sns.distplot(dataframe['cloze probability'])
+    sns.distplot(dataframe['cloze_probability'])
     plt.xlabel('cloze probability', fontsize=20)
     plt.title('Distribution of cloze probability', fontsize=20);
     plt.xticks(fontsize=20)
@@ -142,13 +142,13 @@ def cloze_cort_distribution(dataframe):
         Args:
             dataframe
     """
-    cort_scores = dataframe['Response'].unique()
+    cort_scores = dataframe['CoRT'].unique()
 
     plt.figure(figsize=(10,10))
     # plot histogram of cloze probabilities for each cort scale
     for cort in cort_scores:
     #     plt.figure()
-        sns.kdeplot(dataframe.loc[dataframe['Response']==cort]['cloze probability'], shade=True)
+        sns.kdeplot(dataframe.loc[dataframe['CoRT']==cort]['cloze_probability'], shade=True)
         plt.title(f'Distribution of cloze probabilities', fontsize=20)
         plt.xlabel('cloze probability', fontsize=20)
         plt.legend(cort_scores, fontsize=20)
@@ -163,7 +163,7 @@ def item_analysis(dataframe):
             dataframe
     """
     plt.figure(figsize=(10,10))
-    sns.scatterplot(dataframe.groupby('full_sentence')['Response'].mean(), dataframe.groupby('full_sentence')['Response'].std())
+    sns.scatterplot(dataframe.groupby('full_sentence')['CoRT'].mean(), dataframe.groupby('full_sentence')['Cort'].std())
     plt.xlabel('mean CoRT')
     plt.ylabel('std of CoRT')
     plt.title('item analysis of sentences')
